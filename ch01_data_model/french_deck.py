460e516886463abf69e42ddd80f4848dcd144108
Diego Sorrilha <diego@x4start.com>
2017-10-25 14:53:12 -0200

    implementa funcao que ordena cartas

--- ch01_data_model/french_deck.py
+++ ch01_data_model/french_deck.py
@@ -24,6 +24,15 @@ class FrenchDeck:
         return self._cards[position]
 
 
+# Funcao que classifica as cartas de acordo com regras de valores das mesmas
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+
+
 print('''
 Exemplos de uso:
 >>> deck = FrenchDeck()
@@ -43,4 +52,6 @@ True
 >>> Card('Q', 'beasts') in deck
 False
 
+>>> for card in sorted(deck, key=spades_high):
+...    print(card)
 ''')
